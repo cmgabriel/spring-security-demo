ab7d59c0842d60c420fb9c70629da598056fbcff
cmgabriel <akhileshsharma0316@gmail.com>
2024-12-16 11:38:09 -0500

    Initial Commit

---
+++ src/main/java/com/coreinventor/springsecurity6demo/Springsecurity6demoApplication.java
@@ -0,0 +1,14 @@
+package com.coreinventor.springsecurity6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurity6demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springsecurity6demoApplication.class,
+                args);
+    }
+
+}
---
+++ src/test/java/com/coreinventor/springsecurity6demo/Springsecurity6demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coreinventor.springsecurity6demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurity6demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
